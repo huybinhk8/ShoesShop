544304f03305b0900858fafee1f780f6b24fb004
khanhpraogl <khanhpraogl@gmail.com>
2018-05-31 14:39:09 +0700

    Fix đa ngôn ngữ

--- src/internationalization/message/Language.java
+++ src/internationalization/message/Language.java
@@ -8,7 +8,7 @@ public class Language {
 	
 	public static String getBundles(String key, Locale locale){
 		if(locale == null){
-			bundle = ResourceBundle.getBundle("internationalization.message.MessageBundle", new Locale("en", "US"));
+			bundle = ResourceBundle.getBundle("internationalization.message.MessageBundle", new Locale("vi", "VN"));
 		}else{
 			bundle = ResourceBundle.getBundle("internationalization.message.MessageBundle", locale);
 		}
@@ -16,8 +16,8 @@ public class Language {
 	}
 	public static void main(String[] args) {
 		System.out.println("eng");
-		System.out.println(getBundles("header.home", new Locale("en", "US")));
+		System.out.println(getBundles("title.home", new Locale("en", "US")));
 		System.out.println("vi");
-		System.out.println(getBundles("header.home", new Locale("vi", "VN")));
+		System.out.println(getBundles("title.home", new Locale("vi", "VN")));
 	}
 }
